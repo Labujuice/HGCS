085a98046951a18a6f96d9ec73b647963e7c4461
Labujuice <kennycym@gmail.com>
2026-06-23 22:25:30 +0800

    fix: catch and ignore connection errors in HTTP request handler

--- gateway/gateway.py
+++ gateway/gateway.py
@@ -1182,9 +1182,17 @@ class Gateway:
         
         class Handler(http.server.SimpleHTTPRequestHandler):
             def __init__(self, *args, **kwargs):
-                super().__init__(*args, directory=directory, **kwargs)
+                try:
+                    super().__init__(*args, directory=directory, **kwargs)
+                except (BrokenPipeError, ConnectionResetError, ConnectionAbortedError, OSError):
+                    pass
             def log_message(self, format, *args):
                 pass # Suppress standard HTTP logs to keep stdout clean
+            def handle(self):
+                try:
+                    super().handle()
+                except (BrokenPipeError, ConnectionResetError, ConnectionAbortedError, OSError):
+                    pass
                 
         socketserver.TCPServer.allow_reuse_address = True
         try:
